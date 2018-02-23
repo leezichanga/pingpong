c423e7a646203569cbf13ca1fb951b0be3d9a326
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-02-23 21:07:36 +0300

    scripts method

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,36 @@
-$(document).ready(function() {
- $("form#ping-pong").submit(function(event) {
-event.preventDefault();
- var interger = parseInt($("input#value").val());
- var result = ping pong!(value);
-$("#result").text(result);
- })
-})
 
- // business logic
-var ping pong!= function(value) {
-for (var i=0; i<=30; i++) {
- if (i % 3 === 0) {
-   return "Ping";
- } else if (i % 5 === 0) {
-   return "Pong";
- } else if (i % 15 === 0) {
-   return "Ping Pong!";
- }
+//business logic
+var numbersToBePingponged = [];
+
+function pingPong (number){
+    for (var index = 1; index <= number; index+=1){
+        if(index % 15 === 0){
+            numbersToBePingponged.push("pingpong");
+        }
+        else if(index % 5 === 0){
+            numbersToBePingponged.push("pong");
+        }
+        else if(index % 3 === 0){
+            numbersToBePingponged.push("ping");
+        }
+        else{
+            numbersToBePingponged.push(index);
+        }
+    }
 }
-};
 
-// user interface logic
-$(document).ready(function() {
-$("form#ping-pong").submit(function(event) {
-event.preventDefault();
-var year = parseInt($("input#value").val());
-var result = ping pong!(value);
 
-$("#result").show();
- })
-})
+//user interface logic
+
+$(document).ready(function(){
+   $("form#ping-pong").submit(function(){
+      event.preventDefault();
+      var number = parseInt($("input#number").val());
+
+      pingPong(number);
+
+      numbersToBePingponged.forEach(function(number){
+         $("#output").append('<li>' + number + "</li>");
+      });
+   });
+});
